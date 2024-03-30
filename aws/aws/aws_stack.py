c3004c0ed21428100c3a309331f36cfcac609779
tux <mathieu.barrios@outlook.com>
2024-03-30 20:53:56 +0100

    ajout des fichier créer avec la commande cdk init + commencer à configuer l'environnement aws

--- aws/aws/aws_stack.py
+++ aws/aws/aws_stack.py
@@ -3,15 +3,17 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
 )
-
-from aws_cdk import aws_sagemaker as sagemaker
-
 from constructs import Construct
 
-
-class Jupyter_lab(Stack):
+class AwsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id,**kwargs)
-        # plus qu'a initialiser le Sagemaker ici Doc :https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_sagemaker-readme.html
+        super().__init__(scope, construct_id, **kwargs)
 
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "AwsQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
